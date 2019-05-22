fb1aa377f35a07c5fbd3d3370ed73b9ab3569c3a
David Friedl <david.friedl@whu.edu>
2019-05-22 14:04:39 +0200

    Detail page banner and booking form. Page design for creating a station

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,7 +1,16 @@
 import flatpickr from "flatpickr"
 import "flatpickr/dist/themes/airbnb.css" // Note this is important!
+import "flatpickr/dist/flatpickr.min.css"
+import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
 
+// On booking page
+flatpickr("#range_start", {
+  enableTime: true,
+  time_24hr: true,
+  plugins: [new rangePlugin({ input: "#range_end"})]
+})
 
+// on home page
 flatpickr(".datepicker", {
   enableTime: true,
   allowInput: true,
