a7f8c787adbd36dc4f509cbb901ea1b5e26347bf
Cameron Kolkey <Alleyria@gmail.com>
2019-05-24 16:27:33 +0200

    Works now with scope on station ID

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,5 +3,5 @@ class Booking < ApplicationRecord
   belongs_to :station
   has_many :reviews, dependent: :destroy
   validates :user, :station, :start, :end, presence: true
-  validates :start, :end, overlap: true
+  validates :start, :end, overlap: {scope: :user_id}
 end
