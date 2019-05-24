a97a112b5427489bf1ddd99ade1323245f30d270
David Friedl <david.friedl@whu.edu>
2019-05-24 14:33:30 +0200

    javascript issue fixed

--- app/javascript/components/_review.js
+++ app/javascript/components/_review.js
@@ -1,17 +1,9 @@
-// button.addEventListener('click', (event) => {
-//   event.preventDefault();
-//   event.classList.toggle('hide-review-input-field');
+// const buttonReviewForm = document.querySelector(".form-botton-display");
+
+// const formReview = document.getElementById("review-form-div");
+
+
+// buttonReviewForm.addEventListener('click', (event) => {
+//   formReview.style.display = "flex";
+//   formReview.style.transition = "ease 5s"
 // });
-
-// export {buttonReviewForm};
-
-
-const buttonReviewForm = document.querySelector(".form-botton-display");
-
-const formReview = document.getElementById("review-form-div");
-
-
-buttonReviewForm.addEventListener('click', (event) => {
-  formReview.style.display = "flex";
-  formReview.style.transition = "ease 5s"
-});
